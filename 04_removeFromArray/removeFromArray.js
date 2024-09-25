9eb995fe9610ddbf53e8c6ba2e8a58b6d797456f
DoonSlayer42 <internetsaathihwgo6040@gmail.com>
2024-09-25 17:18:46 +0530

    Solved the 4th problem.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    let len = array.length;
+    let newArray = [];
+    for(let i = 0; i < len; i++) {
+        let willInclude = true; // will include array[i] if this remains true
+        for(const num of toRemove) {
+            if(num === array[i]) {
+                willInclude = false;
+                break;
+            }
+        }
+        if(willInclude) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
