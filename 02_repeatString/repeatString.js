55f4bad03d27c29a78ff7c65a4c8796764c4d541
DoonSlayer42 <internetsaathihwgo6040@gmail.com>
2024-09-25 16:58:27 +0530

    Solved the first and second exercises.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repetition) {
+    if(repetition < 0) {
+        return "ERROR";
+    }
+    let finalString = "";
+    for(let i = 0; i < repetition; i++) {
+        finalString += string;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
