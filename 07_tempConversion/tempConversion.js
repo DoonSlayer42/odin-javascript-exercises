1dceac16192bb1ae41c8de17f9fe140ac902eafc
DoonSlayer42 <internetsaathihwgo6040@gmail.com>
2024-09-25 17:50:23 +0530

    Solved temperature conversion problem.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemperature) {
+  let celsiusTemperature = Number(((fahrenheitTemperature - 32) * (5 / 9)).toFixed(1));
+  return celsiusTemperature;  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemperature) {
+  let fahrenheitTemperature = Number(((9 / 5) * celsiusTemperature + 32).toFixed(1));
+  return fahrenheitTemperature;
 };
 
 // Do not edit below this line
